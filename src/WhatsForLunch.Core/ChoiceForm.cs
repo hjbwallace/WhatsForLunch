00fdbc47de41109b40f9e32ea090d4c5675b9398
Harrison Wallace <hjbwallace@gmail.com>
2020-07-16 19:48:07 +0800

    Updating and simplifying the UI

---
+++ src/WhatsForLunch.Core/ChoiceForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatsForLunch.Core
+{
+    public class ChoiceForm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int? Weighting { get; set; }
+    }
+}
