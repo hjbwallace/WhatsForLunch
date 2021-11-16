5ec88354b52701cc586f92f3b3859e8da5058ba5
Harrison Wallace <hjbwallace@gmail.com>
2021-11-16 22:04:55 +0800

    Updating to NET 6

--- src/WhatsForLunch.Web/Program.cs
+++ src/WhatsForLunch.Web/Program.cs
@@ -1,25 +1,16 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
 using WhatsForLunch.Core;
+using WhatsForLunch.Web;
 using WhatsForLunch.Web.Services;
 
-namespace WhatsForLunch.Web
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddSingleton<AppState>();
-            builder.Services.AddSingleton<IChoiceService, ChoiceService>();
-            builder.Services.AddSingleton<IChoicesRepository, ChoicesRepository>();
+builder.Services.AddSingleton<AppState>();
+builder.Services.AddSingleton<IChoiceService, ChoiceService>();
+builder.Services.AddSingleton<IChoicesRepository, ChoicesRepository>();
 
-            builder.Services.AddBaseAddressHttpClient();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
